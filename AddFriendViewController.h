45055782491b6de655eabd19c521a8ae4051a557
Monkey-D-Mark <332738926@qq.com>
2017-04-19 10:04:11 +0800

    Add files via upload

---
+++ AddFriendViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddFriendViewController.h
+//  QQDRAWER_OC
+//
+//  Created by shendan on 2017/4/19.
+//  Copyright © 2017年 BlackSky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFriendViewController : UIViewController
+
+@end
